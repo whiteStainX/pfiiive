88e0dd1caeb12c31ba5222274166d8067a7cecc4
Huang Song <120066938+whiteStainX@users.noreply.github.com>
2025-10-26 21:21:59 +0800

    update old files (#5)

--- example/backend/run_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-source "$(dirname "$0")/.venv/bin/activate"
-# Change to the directory that needs to be served (the 'example' directory)
-cd "$(dirname "$0")/.."
-# Start the server
-echo "Starting server at http://localhost:8000"
-python3 -m http.server 8000
